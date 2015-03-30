2fa93dcda626858a29c496ef0d868de08febb955
spelka <sebastian.pelka@hotmail.com>
2015-03-29 17:32:32 -0700

    Added full functionality to Menu, but Order.php still doesnt work for some reason. welp!
    Signed-off-by: spelka <sebastian.pelka@hotmail.com>

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -17,30 +17,79 @@ class Menu extends CI_Model {
         parent::__construct();
         $this->xml = simplexml_load_file(DATAPATH . 'menu.xml');
 
-        // build the list of patties - approach 1
-        foreach ($this->xml->patties->patty as $patty) {
-            $patty_names[(string) $patty['code']] = (string) $patty;
-        }
-
         // build a full list of patties - approach 2
-        foreach ($this->$xml->patties->patty as $patty) {
+        foreach ($this->$xml->patties->patty as $patty)
+		{
             $record = new stdClass();
             $record->code = (string) $patty['code'];
             $record->name = (string) $patty;
             $record->price = (float) $patty['price'];
             $patties[$record->code] = $record;
         }
-    }
-
-    // retrieve a list of patties, to populate a dropdown, for instance
-    function patties() {
-        return $this->patty_names;
+		
+		//build a list of cheeses
+        foreach ($this->$xml->cheeses->cheese as $cheese)
+		{
+            $record = new stdClass();
+            $record->code = (string) $cheese['code'];
+            $record->name = (string) $cheese;
+            $record->price = (float) $cheese['price'];
+            $cheeses[$record->code] = $record;
+        }
+		
+		//build a list of toppings
+		foreach ($this->$xml->toppings->topping as $topping)
+		{
+            $record = new stdClass();
+            $record->code = (string) $topping['code'];
+            $record->name = (string) $topping;
+            $record->price = (float) $topping['price'];
+            $toppings[$record->code] = $record;
+        }
+		
+		//build a list of sauces
+		foreach ($this->$xml->sauces->sauce as $sauce)
+		{
+            $record = new stdClass();
+            $record->code = (string) $sauce['code'];
+            $record->name = (string) $sauce;
+            $record->price = (float) $sauce['price'];
+            $sauces[$record->code] = $record;
+        }
     }
 
     // retrieve a patty record, perhaps for pricing
-    function getPatty($code) {
+    function getPatty($code)
+	{
         if (isset($this->patties[$code]))
             return $this->patties[$code];
+        else
+            return null;
+    }
+	
+	// retrieve a cheese record, perhaps for pricing
+    function getCheese($code)
+	{
+        if (isset($this->cheeses[$code]))
+            return $this->cheeses[$code];
+        else
+            return null;
+    }
+	
+	// retrieve a topping record, perhaps for pricing
+    function getTopping($code)
+	{
+        if (isset($this->toppings[$code]))
+            return $this->toppings[$code];
+        else
+            return null;
+    }
+
+	// retrieve a sauce record, perhaps for pricing
+    function getSauce($code)
+	{
+        if (isset($this->sauces[$code]))
+            return $this->sauces[$code];
         else
             return null;
     }
--- application/models/Order.php
+++ application/models/Order.php
@@ -10,8 +10,8 @@ class Order extends CI_Model
 {
 
     protected $xml = null;
-    protected $patty_names = array();
-    protected $patties = array();
+    protected $customer;
+    protected $order;
 
     // Constructor
     public function __construct($filename = NULL) {
@@ -21,24 +21,105 @@ class Order extends CI_Model
 		{
 			return;
 		}
-        $this->xml = simplexml_load_file(DATAPATH . 'menu.xml');
-
-        // build the list of patties - approach 1
-        foreach ($this->xml->patties->patty as $patty)
+        $this->xml = simplexml_load_file(DATAPATH . $filename);
+		
+		$this->customer = (string) $this->xml->customer;
+		$this->order = (string) $this->xml['type'];
+		
+        // for each burger in the XML file, we will parse the data
+        foreach ($this->xml->burger as $burger)
 		{
-            $patty_names[(string) $patty['code']] = (string) $patty;
-        }
-
-        // build a full list of patties - approach 2
-        foreach ($this->xml->patties->patty as $patty)
-		{
-            $record = new stdClass();
-            $record->code = (string) $patty['code'];
-            $record->name = (string) $patty;
-            $record->price = (float) $patty['price'];
-            $patties[$record->code] = $record;
+            $total = 0;
+			$new_burger = new stdClass();
+			
+			//add the patty to the new burger. Adds NULL if it doesnt exist
+			$new_burger->patty = (string) $burger->patty['type'];
+			
+			//check for a patty
+			if ($menu->getPatty($new_burger->patty) != NULL)
+			{
+				//update the price
+				$pattyPrice = $menu->getPatty($new_burger->patty)->price;
+				if ( pattyPrice != NULL)
+				{
+					$total += pattyPrice;
+				}
+			}
+			
+			//add the top cheese to the burger. Adds NULL if it does not exist
+			$new_burger->cheeseTop = (string) $burger->cheeses['top'];	
+			
+			//check for a top layer cheese
+			var_dump($new_burger);
+			if ($menu->getCheese($new_burger->cheeseTop) != NULL)
+			{
+				//update the price
+				$topCheesePrice = (float) $menu->getCheese->($new_burger->cheeseTop)->price;
+				if ($topCheesePrice != NULL)
+				{
+					$total += topCheesePrice;
+				}
+			}
+			
+			//add the bottom cheese to the burger
+			$new_burger->cheeseBottom = (string) $burger->cheeses['bottom'];
+			
+			//check for a bottom layer cheese. Adds NULL if it doesnt exist
+			if ($menu->getCheese($new_burger->CheeseBottom) != NULL)
+			{
+				//update the price
+				$bottomCheesePrice = $menu->getCheese->($new_burger->CheeseBottom)->price;
+				if($bottomCheesePrice != NULL)
+				{
+					$total += bottomCheesePrice;
+				}
+			}
+			
+			//create a new array to hold all the toppings
+			$new_burger->topping = array();
+			
+			//go through all the toppings
+			foreach( $burger->topping as $toppings )
+			{
+				//add the topping to the new burger entry
+				array_push( $new_burger->topping, $toppings['type'] );
+				
+				//update the price
+				$toppingPrice = $menu->getTopping((string)$toppings['type'])->price;
+				if ( $menu->getTopping((string)$toppings['type']) != NULL )
+				{
+					$total += toppingPrice;
+				}
+			}
+			
+			//create a new array to hold all the sauces
+			$new_burger->sauce = array();
+			
+			foreach ( $burger->sauce as $sauces)
+			{
+				//add the sauce to the new burger
+				array_push($new_burger->sauce, $sauces['type']);
+				
+				//update the price
+				$saucePrice = $menu->getSauce((string)$sauces['type'])->price;
+				if ( $menu->getSauce((string)$sauces['type']) != NULL )
+				{
+					$total += saucePrice;
+				}
+			}
+			
+			//assign the running total to the burger
+			$burger->total = $total;
+			
+			//and add the new burger into the order array
+			array_push($this->order, $new_burger);
         }
     }
+	
+	function show_orders()
+	{
+		$file location = directory_map(DATAPATH);
+	}
 
     // retrieve a list of patties, to populate a dropdown, for instance
     function patties()
